87d7940067f13ddb10a8748c203801821b97c560
Aziz Kosber <a.kosber@gmail.com>
2018-05-03 00:08:33 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
 
 def input_to_index(input)
-  index = input.to_i - 1
+  input.to_i - 1
 end
 
 def move(board, index, character = "X")
